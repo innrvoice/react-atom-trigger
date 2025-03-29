d3c634d242c4e0afcf982f745f57951dae567769
innrVoice <pavelbochkov@gmail.com>
2025-03-29 04:56:04 +0300

    v.1.0.16

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,19 @@ import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import commonJS from '@rollup/plugin-commonjs';
 import nodeExternals from 'rollup-plugin-node-externals';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import pkg from './package.json' assert { type: 'json' };
 
 export default {
   input: './src/index.ts',
 
   output: [
-    {
-      format: 'es',
-      file: pkg.module,
-      globals: {
-        react: 'React',
-      },
-    },
+    { format: 'es', file: pkg.module, globals: { react: 'React' } },
     {
       format: 'umd',
       file: pkg.main,
       name: pkg.name,
-      globals: {
-        react: 'React',
-      },
+      globals: { react: 'React' },
     },
   ],
 
